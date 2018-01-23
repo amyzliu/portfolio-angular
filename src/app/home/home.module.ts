542812290cf8b4fc9799505d666bac1a2f20854d
amyzliu <liuzamy@gmail.com>
2018-01-23 15:13:04 -0500

    fixed no show of projects tabset

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { HomeComponent } from './home.component';
 import { MyProjectsComponent } from './../my-projects/my-projects.component';
+import { BlogComponent } from './../blog/blog.component';
 import { FooterComponent } from './../shared/layout/footer/footer.component';
 import { HeaderComponent } from './../shared/layout/header/header.component';
 // import { TabsetComponent } from './../tabset/tabset.component';
@@ -10,8 +11,16 @@ import { HeaderComponent } from './../shared/layout/header/header.component';
 
 const homeRouting: ModuleWithProviders = RouterModule.forChild([
   {
-    path: '',
+    path: 'home',
     component: HomeComponent
+  },
+  {
+    path: 'my-projects',
+    component: MyProjectsComponent
+  },
+  {
+    path: 'blog',
+    component: BlogComponent
   }
 ]);
 
